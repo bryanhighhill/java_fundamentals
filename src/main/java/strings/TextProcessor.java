d42ecf2307f9c75ce0679d9bc8709c385b9830a9
Bryan Highhill <bryanhighhill@gmail.com>
2023-10-26 10:45:18 -0500

    used the StringBuilder class to create a method that inserts a space before each capital letter in a string with no spaces

--- src/main/java/strings/TextProcessor.java
+++ src/main/java/strings/TextProcessor.java
@@ -4,7 +4,8 @@ public class TextProcessor {
 
     public static void main(String[] args) {
 //        countWords("I love Java");
-        reverseString("Hello world");
+//        reverseString("Hello world"); //strings are immutable and cannot be modified
+        addSpaces("HeyWorld!It'sMeBryan");
     }
 
     public static void countWords(String text){
@@ -23,5 +24,16 @@ public class TextProcessor {
         for(int i=text.length()-1; i>=0; i--){
             System.out.print(text.charAt(i));
         }
+    }
+
+    public static void addSpaces(String text){
+        var modifiedText = new StringBuilder(text);
+        for(int i=0; i<modifiedText.length(); i++){
+            if(i!=0 && Character.isUpperCase(modifiedText.charAt(i))){
+                modifiedText.insert(i, " ");
+                i++; //advance i to accomodate for added space
+            }
+        }
+        System.out.println(modifiedText);
     }
 }
